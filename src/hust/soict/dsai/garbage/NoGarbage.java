930692ef1a302292632dc23ad0c0aaf64d38c284
VinhTran <vinhthanhtran03102004@gmail.com>
2024-04-27 20:00:51 +0700

    exercise

--- src/hust/soict/dsai/garbage/NoGarbage.java
+++ src/hust/soict/dsai/garbage/NoGarbage.java
@@ -4,7 +4,6 @@ public class NoGarbage {
     public static void main(String[] args) {
         StringBuilder reusableStringBuilder = new StringBuilder(); // Reusable StringBuilder
         while (true) {
-            // Clear the StringBuilder for reuse
             reusableStringBuilder.setLength(0);
             // Generate some data using the StringBuilder
             reusableStringBuilder.append("Some data");
