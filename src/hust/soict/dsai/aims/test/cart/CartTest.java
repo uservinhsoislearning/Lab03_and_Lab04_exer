eab667560eb478b3419006f46f83ec426bcd9ab1
VinhTran <vinhthanhtran03102004@gmail.com>
2024-04-27 11:45:18 +0700

    completing exercises

--- src/hust/soict/dsai/aims/test/cart/CartTest.java
+++ src/hust/soict/dsai/aims/test/cart/CartTest.java
@@ -1,4 +1,16 @@
 package hust.soict.dsai.aims.test.cart;
 
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
 public class CartTest {
+    public static void main(String[] args) {
+        //Create a new Cart
+        Cart cart = new Cart();
+
+        //Create a new dvd objects and add them to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+        cart.addDigitalVideoDisc(dvd1);
+    }
 }
