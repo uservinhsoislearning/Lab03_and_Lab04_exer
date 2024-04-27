8086cd918c3547d254b34640d255d7b45446d5d6
VinhTran <vinhthanhtran03102004@gmail.com>
2024-04-27 11:46:56 +0700

    completing exercises

--- src/hust/soict/dsai/aims/test/cart/CartTest.java
+++ src/hust/soict/dsai/aims/test/cart/CartTest.java
@@ -12,5 +12,11 @@ public class CartTest {
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
                 "Animation", "Roger Allers", 87, 19.95f);
         cart.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        cart.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation", 18.99f);
+        cart.addDigitalVideoDisc(dvd3);
     }
 }
