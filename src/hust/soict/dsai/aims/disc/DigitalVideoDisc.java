51aa2d185e3d9a68121c1bb10d5c6f8e80232c3f
VinhTran <vinhthanhtran03102004@gmail.com>
2024-04-27 10:43:57 +0700

    completing exercises

--- src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
+++ src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -17,14 +17,26 @@ public class DigitalVideoDisc {
     public String getCategory() {
         return category;
     }
+    public void setCategory(String category) {
+        this.category = category;
+    }
     public String getDirector() {
         return director;
+    }
+    public void setDirector(String director) {
+        this.director = director;
     }
     public int getLength() {
         return length;
     }
+    public void setLength(int length) {
+        this.length = length;
+    }
     public float getCost() {
         return cost;
+    }
+    public void setCost(float cost) {
+        this.cost = cost;
     }
     public DigitalVideoDisc(String title) {
         super();
