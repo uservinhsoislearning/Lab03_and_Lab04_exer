96b9f4127aa61081b0d2755110c79a6802cd2b24
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-17 13:14:22 +0700

    exercise

--- src/hust/soict/dsai/aims/media/Track.java
+++ src/hust/soict/dsai/aims/media/Track.java
@@ -3,5 +3,15 @@ package hust.soict.dsai.aims.media;
 public class Track {
     private String title;
     private int length;
-
+    public Track(String title, int length) {
+        super();
+        this.title = title;
+        this.length = length;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public int getLength() {
+        return length;
+    }
 }
