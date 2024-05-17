843c2d84cbba6a4a23c7aac52568405bf710ac53
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-17 13:07:09 +0700

    exercise

--- src/hust/soict/dsai/aims/media/Disc.java
+++ src/hust/soict/dsai/aims/media/Disc.java
@@ -3,4 +3,10 @@ package hust.soict.dsai.aims.media;
 public class Disc extends Media {
     protected float length;
     protected String director;
+    public float getLength() {
+        return length;
+    }
+    public String getDirector() {
+        return director;
+    }
 }
