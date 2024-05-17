6e130b28d9745fa1f4826c8e83ff3e5caf01e4d8
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-17 13:23:46 +0700

    exercise

--- src/hust/soict/dsai/aims/media/CompactDisc.java
+++ src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -5,5 +5,31 @@ import java.util.ArrayList;
 public class CompactDisc extends Disc{
     private String artist;
     private ArrayList<Track> tracks = new ArrayList<Track>();
-
+    public String getArtist() {
+        return artist;
+    }
+    public void addTrack(Track newtrack) {
+        boolean checked = false;
+        for (Track track : tracks){
+            if (newtrack.equals(track)){
+                checked = true;
+                break;
+            }
+        }
+        if (!checked){
+            tracks.add(newtrack);
+        }
+    }
+    public void removeTrack(Track oldtrack) {
+        boolean checked = false;
+        for (Track track : tracks){
+            if (oldtrack.equals(track)){
+                checked = true;
+                break;
+            }
+        }
+        if (checked){
+            tracks.remove(oldtrack);
+        }
+    }
 }
