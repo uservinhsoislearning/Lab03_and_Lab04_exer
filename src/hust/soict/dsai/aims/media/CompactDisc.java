521ed7c583bacc06b0841233245a678e6b391645
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-17 13:35:58 +0700

    exercise

--- src/hust/soict/dsai/aims/media/CompactDisc.java
+++ src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -2,7 +2,7 @@ package hust.soict.dsai.aims.media;
 
 import java.util.ArrayList;
 
-public class CompactDisc extends Disc{
+public class CompactDisc extends Disc implements Playable{
     private String artist;
     private ArrayList<Track> tracks = new ArrayList<Track>();
     public String getArtist() {
@@ -32,13 +32,18 @@ public class CompactDisc extends Disc{
             tracks.remove(oldtrack);
         }
     }
-    public float getLength(){
-        float totallength = 0;
+    @Override
+    public int getLength(){
+        int totallength = 0;
         for (Track track : tracks){
             if (track.getLength() > 0){
                 totallength += track.getLength();
             }
         }
         return totallength;
+    }
+    public void play(){
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
     }
 }
