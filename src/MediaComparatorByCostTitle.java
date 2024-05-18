b40be51401c9b7b686c6de99c734ac4af0a6fb04
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-18 20:23:57 +0700

    exercise

--- src/MediaComparatorByCostTitle.java
+++ src/MediaComparatorByCostTitle.java
@@ -3,7 +3,11 @@ import hust.soict.dsai.aims.media.Media;
 
 public class MediaComparatorByCostTitle implements Comparator<Media> {
     @Override
-    public int compare(Media o1, Media o2) {
-        return 0;
+    public int compare(Media m1, Media m2) {
+            return Comparator
+                    .comparing((Media media) -> -media.getCost())
+                    .thenComparing(media -> media.getTitle().toLowerCase())
+                    .compare(m1, m2);
+        }
     }
 }
