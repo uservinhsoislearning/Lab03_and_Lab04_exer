3154acd28e73bec933566a657fb07909e2e5fb1a
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-18 20:50:59 +0700

    exercise

--- src/hust/soict/dsai/aims/media/CompactDisc.java
+++ src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class CompactDisc extends Disc implements Playable{
     private String artist;
     private ArrayList<Track> tracks = new ArrayList<Track>();
+    public CompactDisc(String title, String category, String director, float cost, String artist, ArrayList<Track> tracks) {
+        super();
+        this.artist = artist;
+        this.tracks = tracks;
+    }
     public String getArtist() {
         return artist;
     }
