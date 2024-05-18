5d9e1ad1ed52626338e78a72982a5c9b466281e6
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-18 20:56:46 +0700

    exercise

--- src/hust/soict/dsai/aims/media/Book.java
+++ src/hust/soict/dsai/aims/media/Book.java
@@ -6,6 +6,17 @@ import java.util.ArrayList;
 public class Book extends Media{
 
     private List<String> authors = new ArrayList<String>();
+    public Book(){
+        super();
+    }
+    public Book(String title, String category, float cost) {
+        super();
+    }
+
+    public Book(String title, String category, float cost, List<String> authorList) {
+        super();
+        authors.addAll(authorList);
+    }
     public List<String> getAuthors() {
         return authors;
     }
@@ -13,9 +24,7 @@ public class Book extends Media{
     public void setAuthors(List<String> authors) {
         this.authors = authors;
     }
-    public Book(){
 
-    }
     public void addAuthor(String authorName){
         boolean checked = false;
         for (String name : authors){
