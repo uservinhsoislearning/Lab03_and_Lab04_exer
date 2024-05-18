99becb3bb9fd575fa9c3ea7e24f9be13a1c9d640
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-18 20:32:20 +0700

    exercise

--- src/hust/soict/dsai/aims/Aims.java
+++ src/hust/soict/dsai/aims/Aims.java
@@ -8,6 +8,51 @@ import hust.soict.dsai.aims.media.Book;
 import java.util.*;
 
 public class Aims {
+    public static void showMenu() {
+        System.out.println("AIMS: ");
+        System.out.println("--------------------------------");
+        System.out.println("1. View store");
+        System.out.println("2. Update store");
+        System.out.println("3. See current cart");
+        System.out.println("0. Exit");
+        System.out.println("--------------------------------");
+        System.out.println("Please choose a number: 0-1-2-3");
+    }
+    public static void storeMenu() {
+
+        System.out.println("Options: ");
+        System.out.println("--------------------------------");
+        System.out.println("1. See a media’s details");
+        System.out.println("2. Add a media to cart");
+        System.out.println("3. Play a media");
+        System.out.println("4. See current cart");
+        System.out.println("0. Back");
+        System.out.println("--------------------------------");
+        System.out.println("Please choose a number: 0-1-2-3-4");
+
+    }
+    public static void mediaDetailsMenu() {
+        System.out.println("Options: ");
+        System.out.println("--------------------------------");
+        System.out.println("1. Add to cart");
+        System.out.println("2. Play");
+        System.out.println("0. Back");
+        System.out.println("--------------------------------");
+        System.out.println("Please choose a number: 0-1-2");
+    }
+    public static void cartMenu() {
+        System.out.println("Options: ");
+        System.out.println("--------------------------------");
+        System.out.println("1. Filter medias in cart");
+        System.out.println("2. Sort medias in cart");
+        System.out.println("3. Remove media from cart");
+        System.out.println("4. Play a media");
+        System.out.println("5. Place order");
+        System.out.println("0. Back");
+        System.out.println("--------------------------------");
+        System.out.println("Please choose a number: 0-1-2-3-4-5");
+
+    }
     public static void main(String[] args) {
 
 //        Cart anOrder = new Cart();
@@ -23,19 +68,19 @@ public class Aims {
 //        anOrder.addDigitalVideoDisc(dvd3);
 //
 //        System.out.println("Total cost is: " + anOrder.totalCost());
-        List<Media> mediae = new ArrayList<Media>();
-
-        CompactDisc cd = new CompactDisc();
-        DigitalVideoDisc dvd = new DigitalVideoDisc("The Lion King",
-                "Animation", "Roger Allers", 87, 19.95f);
-        Book book = new Book();
-
-        mediae.add(cd);
-        mediae.add(dvd);
-        mediae.add(book);
-
-        for (Media media : mediae) {
-            System.out.println(media.toString());
-        }
+//        List<Media> mediae = new ArrayList<Media>();
+//
+//        CompactDisc cd = new CompactDisc();
+//        DigitalVideoDisc dvd = new DigitalVideoDisc("The Lion King",
+//                "Animation", "Roger Allers", 87, 19.95f);
+//        Book book = new Book();
+//
+//        mediae.add(cd);
+//        mediae.add(dvd);
+//        mediae.add(book);
+//
+//        for (Media media : mediae) {
+//            System.out.println(media.toString());
+//        }
     }
 }
