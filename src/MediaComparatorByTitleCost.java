12d7379ad84241f919e77e9967e5a5cd7f5b5ee1
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-18 20:26:55 +0700

    exercise

--- src/MediaComparatorByTitleCost.java
+++ src/MediaComparatorByTitleCost.java
@@ -5,8 +5,8 @@ public class MediaComparatorByTitleCost implements Comparator<Media> {
     @Override
     public int compare(Media m1, Media m2) {
         return Comparator
-                .thenComparing(media -> media.getTitle().toLowerCase())
-                .comparing((Media media) -> -media.getCost())
+                .comparing((Media media) -> media.getTitle().toLowerCase())
+                .thenComparing(media -> -media.getCost())
                 .compare(m1, m2);
     }
 }
