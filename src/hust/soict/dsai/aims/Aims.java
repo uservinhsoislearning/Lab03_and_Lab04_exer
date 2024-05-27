1dfb4762a29f05fa65241d69f895b18ce0ba3468
VinhTran <vinhthanhtran03102004@gmail.com>
2024-05-27 21:23:13 +0700

    exercise

--- src/hust/soict/dsai/aims/Aims.java
+++ src/hust/soict/dsai/aims/Aims.java
@@ -74,6 +74,19 @@ public class Aims {
         }
 
     }
+    public static void cartMenu() {
+        System.out.println("Options: ");
+        System.out.println("--------------------------------");
+        System.out.println("1. Filter medias in cart");
+        System.out.println("2. Sort medias in cart");
+        System.out.println("3. Remove media from cart");
+        System.out.println("4. Play a media");
+        System.out.println("5. Place order");
+        System.out.println("0. Back");
+        System.out.println("--------------------------------");
+        System.out.println("Please choose a number: 0-1-2-3-4-5");
+
+    }
     public static void storeMenu() {
 
         System.out.println("Options: ");
@@ -151,19 +164,6 @@ public class Aims {
         System.out.println("0. Back");
         System.out.println("--------------------------------");
         System.out.println("Please choose a number: 0-1-2");
-    }
-    public static void cartMenu() {
-        System.out.println("Options: ");
-        System.out.println("--------------------------------");
-        System.out.println("1. Filter medias in cart");
-        System.out.println("2. Sort medias in cart");
-        System.out.println("3. Remove media from cart");
-        System.out.println("4. Play a media");
-        System.out.println("5. Place order");
-        System.out.println("0. Back");
-        System.out.println("--------------------------------");
-        System.out.println("Please choose a number: 0-1-2-3-4-5");
-
     }
     public static void exit() {
         System.out.println("Exiting...");
